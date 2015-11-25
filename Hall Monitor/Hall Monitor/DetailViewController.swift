7186f749b21093bf0d28a2bccfb4a91323a2e177
Chris Grant <chris@mc-130-49-52-129.oakland.resnet.pitt.edu>
2015-11-25 17:47:15 -0500

    Added Map

--- Hall Monitor/Hall Monitor/DetailViewController.swift
+++ Hall Monitor/Hall Monitor/DetailViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
-
+import MapKit
 class DetailViewController: UIViewController {
 
     @IBOutlet weak var detailDescriptionLabel: UILabel!
 
 
+    @IBOutlet weak var mapView: MKMapView!
     var detailItem: AnyObject? {
         didSet {
             // Update the view.
@@ -24,11 +25,18 @@ class DetailViewController: UIViewController {
     override func viewWillAppear(animated: Bool) {
         detailDescriptionLabel.text = choice
     }
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+            regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         print(choice)
+        let initialLocation = CLLocation(latitude: 40.444271, longitude:-79.953291)
         // Do any additional setup after loading the view, typically from a nib.
-       
+       centerMapOnLocation(initialLocation)
     }
 
     override func didReceiveMemoryWarning() {
